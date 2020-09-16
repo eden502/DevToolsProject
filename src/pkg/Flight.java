09f8d9a03354673a856723dc8b85592d03762b50
eden502 <62388816+eden502@users.noreply.github.com>
2020-09-16 18:51:02 +0300

    Added C'tor to 'Flight' class

--- src/pkg/Flight.java
+++ src/pkg/Flight.java
@@ -1,5 +1,39 @@
 package pkg;
 
+import java.time.LocalDateTime;
+
 public class Flight {
+	
+	
+	private int tailNum;
+	private String company;
+	private String APDest;
+	private String APOrigin;
+	private String CTDest;
+	private String CTOrigin;
+	private String CDest;
+	private String COrigin;
+	private double flightLength;
+	private LocalDateTime flightTime;
+	public enum flightClass {Arrival, Departure};
+	private flightClass type;
+	
+	
+	public Flight(int tailNum, String company, String APDest, String APOrigin, String CTDest, String CTOrigin, String CDest, String COrigin, LocalDateTime flightTime, flightClass type) {
+		this.tailNum = tailNum;
+		this.company = company;
+		this.APDest = APDest;
+		this.APOrigin = APOrigin;
+		this.CTDest = CTDest;
+		this.CTOrigin = CTOrigin;
+		this.CDest = CDest;
+		this.COrigin = COrigin;
+		this.flightTime = flightTime;
+		this.type = type;
+		
+		
+	}
+	
+	
 
 }
